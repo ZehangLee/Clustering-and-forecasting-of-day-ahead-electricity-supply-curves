6a008055051d5d65cc1b980954c0f481676c6a04
ZehangLee <304025li@gmail.com>
2022-04-18 18:54:29 +0200

    add WSS procedure to decide the best number of clusters

--- day_ahead_weighted_distance.R
+++ day_ahead_weighted_distance.R
@@ -5,6 +5,8 @@ library(ffbase)
 library(tidyverse)
 library(mixtools)
 library(LaplacesDemon)
+library(cluster)
+library(tictoc)
 
 
 ffload(file="D:/Script/R_script/day_ahead market/day_ahead market")
@@ -95,17 +97,17 @@ Prices = as.matrix(day_ahead_supply_price[1:n_curves,3:700])
 Quants = as.matrix(day_ahead_supply_cumsum[1:n_curves,3:700])
 
 
-pp = as.vector(Prices)
-pp = na.omit(pp)
-pp = pp[pp<127]  #127.4305
+# pp = as.vector(Prices)
+# pp = na.omit(pp)
+# pp = pp[pp<127]  #127.4305
 
 
-mixmdl = normalmixEM(pp, k = 2, maxrestarts = 1000, verb = FALSE)
+#mixmdl = normalmixEM(pp, k = 2, maxrestarts = 1000, verb = FALSE)
 mixture.gaussian <- function(x) dnormm(x, p = c(mixmdl$lambda[1],1-mixmdl$lambda[1]), mu = mixmdl$mu, sigma = mixmdl$sigma)
 
 
-start =  1
-end = 100#950e6
+start =  835e6 + 1
+end = 840e6
 inds_sub= inds_ff[,start:end]
 
 
@@ -164,27 +166,35 @@ close(pb)
 stopCluster(cl) 
 
 
-save_file = paste("dist",floor(end/1e6),".RDS",sep = "")
+save_file = paste("D:/Script/R_script/day_ahead market/day_ahead market/dist/dist",floor(end/1e6),".RDS",sep = "")
 saveRDS(dist,file = save_file)
 
 
 
 ###################################################################################
-path = "distance/1-310"
-dist_list310 = list.files(path=path,pattern = ".RDS",full.names = T) %>% 
-  map(readRDS) %>%
-  unlist(use.names = F)
+# path = "distance/1-310"
+# dist_list310 = list.files(path=path,pattern = ".RDS",full.names = T) %>% 
+#   map(readRDS) %>%
+#   unlist(use.names = F)
 
 
-path = "distance"
-dist_list_after_310 = list.files(path=path,pattern = ".RDS",full.names = T) %>% 
+path = "D:/Script/R_script/day_ahead market/day_ahead market/dist"
+dist_list = list.files(path=path,pattern = ".RDS",full.names = T) %>% 
   map(readRDS) %>%
   unlist(use.names = F) 
+#########################################################################
 
 
+# 
+# d = dist_list
+# rm(dist_list)
+# gc()
+# 
+# d = d/264297298.395996 # Divide by sd(Quants)^2
+# d = sqrt(d) 
 
-
-d = dist_list
+# d_ff = as.ff(d)
+# ffsave(d_ff,file="D:/Script/R_script/day_ahead market/day_ahead market",add = T)
 
 attributes(d) <- list(Size = 43848,
                       Labels = as.character(1:43848),
@@ -194,6 +204,141 @@ attributes(d) <- list(Size = 43848,
 
 class(d) = "dist"
 
-result=hclust(d)
+result=hclust(d,method = "average")
+
 result$merge
 
+
+
+quantile(result$height)
+plot(density(result$height)) #the maxmum is such large that narrow the density curve into a peak
+med = median(result$height)
+plot(density(result$height[result$height<med]))
+plot(density(log(result$height)))
+
+
+groups <- cutree(result, k=5)
+table(groups)
+
+plot(result)
+rect.hclust(result, k=5, border=1:5)
+
+
+require(viridis)
+require(dendextend)
+
+# to visualize every subdendrogram
+dend <- result %>%as.dendrogram %>%  
+  set("labels_to_character") %>% color_branches(k=5)
+dend_list <- get_subdendrograms(dend, 5)
+
+
+par(mfrow = c(2,3))
+plot(dend, main = "Original dendrogram")
+sapply(dend_list, plot)
+
+#############################################################################
+
+# select best number of cluster using Average Silhouette Method
+hc <- result
+
+avg_sil = function(k) {
+  cut_res = cutree(hc,k)
+  ss = silhouette(cut_res, d)
+  mean(ss[, 3])
+}
+
+k.values <- 2:15
+
+# extract avg silhouette for 2-15 clusters
+avg_sil_values <- sapply(k.values, avg_sil)
+
+plot(k.values, avg_sil_values,
+     type = "b", pch = 19, frame = FALSE, 
+     xlab = "Number of clusters K",
+     ylab = "Average Silhouettes")
+
+
+
+# select best number of cluster using Within-Cluster-Sum of Squared Errors (WSS)
+
+k = 100
+cut_res = cutree(result,k)
+cluster_memb = list()
+
+for(i in 1:k){
+  cluster_memb[[i]] = unname(which(cut_res==i))
+}
+
+
+save_file = paste("D:/Script/R_script/day_ahead market/day_ahead market/memb_in_cluster/cluster_memb_k_",k,".RDS",sep = "")
+saveRDS(cluster_memb,file = save_file)
+
+
+
+## 1. find centroid  of each clusters
+
+# m is the number shown in the pairs (,m) and (m,)
+# n is the size
+find_index = function(m,n){
+  
+  if(m ==1){
+    index = 1:(n-1)
+  }
+  else{
+    
+    first_index= m-1
+    k = m-1
+    
+    gap = rep(n-m, k-1)
+    offset = (m-2):1
+    index1 = c(first_index, first_index + cumsum(gap + offset)) # all index of (,m)
+    
+    index2 = index1[length(index1)] + (n-m) + 1:(n-m) # all index of (m,)
+    index = c(index1, index2)
+  }
+  return(index)
+}
+
+
+
+
+cluster_num = 100
+path = paste("D:/Script/R_script/day_ahead market/day_ahead market/memb_in_cluster/cluster_memb_k_",cluster_num,".RDS",sep = "")
+cluster_memb = readRDS(path)
+cluster_stat = list()
+
+for(cl in 1:cluster_num){
+  memb_num= length(cluster_memb[[cl]])
+  
+  dist2neighbor = c()
+  
+  
+  for(i in 1:memb_num){
+    print(paste(cl,"-",i/memb_num))
+    index1 = cluster_memb[[cl]][i]
+    index2 = cluster_memb[[cl]][-i]
+    index2 = index2 - (index2 > index1)
+    
+    location_in_dist_m = find_index(index1, 43848)[index2]
+    dist2neighbor[i]= sum(d_ff[location_in_dist_m])
+  }
+  
+  centroid_index = which.min(dist2neighbor)
+  centroid = cluster_memb[[cl]][centroid_index]
+  WSS = dist2neighbor[centroid_index]
+  cluster_stat[[cl]]= c(centroid,WSS)
+}
+
+
+
+save_file = paste("D:/Script/R_script/day_ahead market/day_ahead market/cluster_stat_cl",cluster_num,".RDS",sep = "")
+saveRDS(cluster_stat,file = save_file)
+
+
+stats = paste("cluster_stat_cl",c(2,seq(10,100,10)),sep = "") 
+
+Total_WSS = sapply(1:length(stats), function(k)  {
+  x = get(stats[k])
+  sum(sapply(1:length(x),function(i) x[[i]][2]))})
+plot(c(2,seq(10,100,10)),Total_WSS,type = "b")
